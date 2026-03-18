737b3accbba2f1cd4b00a06fa6fe070639603552
arhamcode <arham.code@gmail.com>
2026-03-18 09:24:29 +0800

    Update tailwind installation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: 'https://arhamcode.github.io',
+  site: "https://arhamcode.github.io",
+
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'id'],
+    defaultLocale: "en",
+    locales: ["en", "id"],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-  integrations: [
-    tailwind(),
-    mdx()
-  ],
+
+  integrations: [mdx()],
+
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
-      wrap: true
-    }
-  }
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
